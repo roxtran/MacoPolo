f497c53c9d4236827b18699b4ac51679cea4192a
Roxanne Tran <gdtran@myseneca.ca>
2020-11-20 03:57:34 -0500

    shipping, placeorder, create order

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,8 @@ import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 
 const App = () => {
   return (
@@ -19,6 +21,8 @@ const App = () => {
         <Container>
           <Route path='/login' component={LoginScreen} />
           <Route path='/shipping' component={ShippingScreen} />
+          <Route path='/payment' component={PaymentScreen} />
+          <Route path='/placeorder' component={PlaceOrderScreen} />
           <Route path='/register' component={RegisterScreen} />
           <Route path='/profile' component={ProfileScreen} />
           <Route path='/product/:id' component={ProductScreen} />
